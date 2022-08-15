50f78f2918abff5e3fe2506e6932f1d4e3d70a67
rebeccalpeace <rebeccalpeace@gmail.com>
2022-08-15 15:58:23 -0400

    added app

---
+++ app/routes.py
@@ -0,0 +1,17 @@
+from app import app
+from flask import render_template
+
+
+@app.route('/')
+def index():
+    user_info = {
+        'username': 'rebeccap',
+        'email': 'rpeace@gmail.com'
+    }
+    colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+    return render_template('index.html', user=user_info, colors=colors)
+
+
+@app.route('/test')
+def test():
+    return 'This is a test'
