a5e4489b43ebdcd4f6eac497e35b17f877f2e315
rebeccalpeace <rebeccalpeace@gmail.com>
2022-08-16 16:56:15 -0400

    added init to User class

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@
 
 from app import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash
 
 
 class User(db.Model):
@@ -11,3 +12,12 @@ class User(db.Model):
     username = db.Column(db.String(50), nullable=False, unique=True)
     password = db.Column(db.String(256), nullable=False)
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # save the password as the hashed version of the password
+        self.password = generate_password_hash(kwargs['password'])
+        db.session.add(self)
+        db.session.commit()
+
+        
